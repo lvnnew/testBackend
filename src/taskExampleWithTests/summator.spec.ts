2d6367ac244a0788d20d3bda070437e0f93d8c48
Damir <tebuchet@gmail.com>
2022-06-10 21:48:17 +0300

    fix

--- src/taskExampleWithTests/summator.spec.ts
+++ src/taskExampleWithTests/summator.spec.ts
@@ -4,11 +4,11 @@ import summator from './summator';
 // yarn jest --testPathPattern summator
 
 describe('summator', () => {
-  it('Суммирует положительные числа', () => {
+  it('суммирует положительные числа', () => {
     expect(summator(4, 5)).toBe(9);
   });
 
-  it('Суммирует отрицительное число с положительным', () => {
+  it('суммирует отрицительное число с положительным', () => {
     expect(summator(-4, 5)).toBe(1);
   });
 });
